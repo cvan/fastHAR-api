82ddc9f4d11e07512553ecbb9802eec87ddd1dfd
Christopher Van <cvan@mozilla.com>
2014-01-22 16:24:13 -0800

    use levelhud for local development

--- db.js
+++ db.js
@@ -1,8 +1,15 @@
 var level = require('level');
 var levelplus = require('levelplus');
 
+var settings = require('./settings_local');
+
 
 module.exports = levelplus(level('./db/har', {
     keyEncoding: 'utf8',
     valueEncoding: 'json'
 }));
+
+if (settings.DEBUG) {
+    var levelHUD = require('levelhud');
+    new levelHUD().use(module.exports).listen(4420);
+}
